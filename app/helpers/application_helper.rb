9fe57cbdd7e2f278787b536233d840dd533a7d97
anmishra <anindya.mishra@perceptivesoftware.com>
2014-09-23 13:11:55 +0530

    1. Set up default time zone 2. Implemented user specific time zone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
   end
   
   def display_datettime(dt)
-    dt.strftime('%m/%d/%Y %l:%M%P %Z')
+    dt = dt.in_time_zone(current_user.time_zone) if logged_in? and !current_user.time_zone.blank?
+    dt.strftime('%m/%d/%Y %l:%M%P %Z') # 09/23/2014 12:43pm
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922121529) do
+ActiveRecord::Schema.define(version: 20140923070057) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140922121529) do
     t.string "password_digest"
     t.string "slug"
     t.string "role"
+    t.string "time_zone"
   end
 
   create_table "votes", force: true do |t|
