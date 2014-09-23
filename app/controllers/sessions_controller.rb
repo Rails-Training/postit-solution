e25c1d86ffa56e569778f5497b9b9cb84f1e7e48
anmishra <anindya.mishra@perceptivesoftware.com>
2014-09-23 17:35:36 +0530

    Implemented two factor authentication with Twilio

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,21 @@
 class SessionsController < ApplicationController
   
+  before_action
+  
   def new
-    
   end
   
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:notice] = "Login successful"
-      redirect_to root_path
+      if user.two_factor_auth?
+        session[:two_factor] = true 
+        user.generate_pin!
+        user.send_pin_to_twilio
+        redirect_to pin_path
+      else
+        login_user! user        
+      end
     else
       flash[:error] = "Invalid username or password"
       redirect_to login_path
@@ -20,6 +26,30 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = "Successfully logged out"
     redirect_to root_path
-  end  
+  end
+  
+  def pin
+    access_denied if session[:two_factor].nil?
+    if request.post?
+      user = User.find_by pin: params[:pin]
+      if user
+        user.remove_pin!
+        login_user! user
+      else
+        flash[:error] = 'Authentication failed'
+        redirect_to pin_path
+      end 
+    end
+  end
+  
+  private
+  
+  def login_user!(user)
+    session[:two_factor] = nil
+    session[:user_id] = user.id
+    flash[:notice] = "Login successful"
+    redirect_to root_path
+  end 
+  
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,50 @@
 class User < ActiveRecord::Base
   include SluggableLexSep
-  
+
   has_many :posts, foreign_key: :user_id
   has_many :comments, foreign_key: :user_id
   has_many :votes, foreign_key: :user_id
-  
+
   has_secure_password validations: false
-  
+
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
-  
+
   sluggable_column :username
-      
   def admin?
     self.role != nil && self.role.downcase == 'admin'
   end
-  
+
   def moderator?
     self.role != nil && self.role.downcase == 'moderator'
   end
-  
+
+  def two_factor_auth?
+    !self.phone.blank?
+  end
+
+  def generate_pin!
+    self.update_column(:pin, rand(10**6))
+  end
+
+  def remove_pin!
+    self.update_column(:pin, nil)
+  end
+
+  def send_pin_to_twilio
+    msg='Please provide the pin to input login: #{self.pin}'
+    # put your own credentials here
+    account_sid = 'AC3a39baf17b1691bb1ea61f605952638b'
+    auth_token = 'aeb63e6e0ea0eac34f989e7ca280b50f'
+
+    # set up a client to talk to the Twilio REST API
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    client.account.messages.create({
+      :from => '+16094546190',
+      :to => '+919007110250',
+      :body => msg
+    })
+  end
+
 end
